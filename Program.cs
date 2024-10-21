51d1318881b3d4438b414880b947107b78902810
theMargarita <margotvoistinova@hotmail.se>
2024-10-21 21:05:59 +0200

    sphere area

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@
 
             Circle circle2 = new Circle(6);
             Console.WriteLine(circle2.CircleArea());
+
+            Circle sphere1 = new Circle(5);
+            Console.WriteLine(sphere1.SphereArea());
+
+            Circle sphere2 = new Circle(6);
+            Console.WriteLine(sphere2.SphereArea());
         }
     }
 }
