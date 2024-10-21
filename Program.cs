f013a0cf4bc1950a1b0cd485cfddd1e51c8250a3
theMargarita <margotvoistinova@hotmail.se>
2024-10-21 20:47:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CircleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
