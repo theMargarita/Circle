c74dd3ed1dcbb32622565aa8ad70ce7a5cd288c2
theMargarita <margotvoistinova@hotmail.se>
2024-10-21 21:17:45 +0200

    triangle area

---
+++ Triangle.cs
@@ -0,0 +1,22 @@
+﻿namespace CircleArea
+{
+    internal class Triangle
+    {
+        public double Base { get; set; }
+        public double Hight { get; set; }
+
+        //constructor with two parameter
+        public Triangle(double basen, double hight)
+        {
+            Base = basen;
+            Hight = hight;
+        }
+
+        //mthod - triangle area
+        public double TriangleArea()
+        {
+            double area = (Base * Hight) / 2;
+            return area;
+        }
+    }
+}
